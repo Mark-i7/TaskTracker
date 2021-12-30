1e1abd0e1de6878637d067a909163a9430be463f
Mark-i7 <markforsapi@gmail.com>
2021-12-30 13:02:52 +0200

    Edited Fragment Create_Board UI

--- TrelloClone/app/src/main/java/com/example/trelloclone/CreateBoard.kt
+++ TrelloClone/app/src/main/java/com/example/trelloclone/CreateBoard.kt
@@ -1,13 +1,21 @@
 package com.example.trelloclone
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.trelloclone.model.Board
+
 
 class CreateBoard : Fragment() {
 
+    private var mSelectedImageFileUri: Uri? = null
+    private lateinit var mUserName:  String
+    private lateinit var mBoardDetails: Board
+    private var mBoardImageURL: String = ""
+    private var isBoardUpdate = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +27,7 @@ class CreateBoard : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_create_board, container, false)
+
     }
+
 }
